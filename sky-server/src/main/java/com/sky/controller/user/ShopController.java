6ecd4f6b0e842c53ec7a07eb25dc037b67977484
Lina <wulinthu@gmail.com>
2024-04-24 00:36:39 +0100

    add user shop status;config wechat appid & key

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,4 +1,4 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
@@ -8,9 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("admin")
+@RestController("user")
 @Api(tags = "shop interfaces")
-@RequestMapping("/admin/shop")
+@RequestMapping("/user/shop")
 @Slf4j
 public class ShopController {
     @Autowired
